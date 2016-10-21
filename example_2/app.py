3210afda3fe1f4cc8ebaea6d42c7fdef1eaf734c
IuryAlves <satriani-16@hotmail.com>
2016-10-20 22:19:14 -0200

    improve example 2

--- example_2/app.py
+++ example_2/app.py
@@ -12,27 +12,32 @@ def home():
     return jsonify(
         {
             'API': [
-                '/close_match',
+                '/closest_word',
                 '/proximity'
             ]
         }
     )
 
 
-@app.route('/close_match', methods=('GET', ))
-def close_match():
+@app.route('/closest_word', methods=('GET', ))
+def closest_word():
     possibilities = request.args.get('possibilities').split(',')
     word = request.args.get('word')
+    try:
+        closest_word = get_close_matches(word, possibilities, n=1)[0]
+    except IndexError:
+        closest_word = 'No matches'
     return jsonify({
-        'result': get_close_matches(word, possibilities, n=1)
+        'closest_word': closest_word
     })
 
 
 @app.route('/proximity', methods=('GET', ))
 def proximity():
     word_a, word_b = request.args.get('word_a'), request.args.get('word_b')
+    proximity = SequenceMatcher(a=word_a, b=word_b).ratio()
     return jsonify({
-        'result': SequenceMatcher(a=word_a, b=word_b).ratio()
+        'proximity': proximity
     })
 
 
